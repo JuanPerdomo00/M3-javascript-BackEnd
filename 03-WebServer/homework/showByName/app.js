5312137cac3773046ad4ac7904a2c3bb7c7fc0ca
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-18 21:40:54 -0500

    terminada home

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,12 @@
-var fs  = require("fs")
-var http  = require("http")
+let http = require("http");
+let { verificPath } = require("./conditionals");
+const PORT = 9100;
 
 // Escribí acá tu servidor
+const server = http.createServer((req, res) => {
+  let { url } = req;
+
+  verificPath(url, req, res);
+});
+server.listen(PORT, "127.0.0.1");
+console.log(`Servidor escuchando en: http://127.0.0.1:${PORT}`);
