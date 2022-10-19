49d2f1482cc5823c33acd59f3789b744661f59b2
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-19 09:09:47 -0500

    terminada la parte de iniciar el servidor

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,12 @@
-var http = require('http');
-var fs   = require('fs');
+const http = require("http");
+const { routes } = require("./routes");
+const PORT = 9090;
 
-var beatles=[{
-  name: "John Lennon",
-  birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
-},
-{
-  name: "Paul McCartney",
-  birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
-},
-{
-  name: "George Harrison",
-  birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
-}
-]
+const server = http.createServer((req, res) => {
+  let { url, method } = req;
+
+  routes(req, res, url, method);
+});
+
+server.listen(PORT, "127.0.0.1");
+console.log(`server listener in http:127.0.0.1:${PORT}`);
