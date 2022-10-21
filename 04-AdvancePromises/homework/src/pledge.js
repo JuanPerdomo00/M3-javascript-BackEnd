e51107b4dc9db5dacc3fb69d9c91da72e2a1554a
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-21 10:23:56 -0500

    capitulo 1 hecho por jakepys

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,10 +1,49 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor) {
+  if (typeof executor !== "function") {
+    throw new TypeError("executor not a function");
+  }
+  this._state = "pending";
+  this._value = undefined;
+  executor(
+    (resolve) => {
+      this._internalResolve(resolve);
+    },
+    (reject) => this._internalReject(reject)
+  );
+}
 
+$Promise.prototype._internalResolve = function (data) {
+  // this.keysArgsName = Object.keys(data);
+  // this.valuesArgs = Object.values(data);
+  // this._value = data
+  // this.__cache = [];
 
+  // this._value = data[keysArgsName[0]];
+
+  // if(this.__cache.length === 0) {
+  //   this.__cache.push(this._value)
+  //   this._state = "pending"
+  //   return
+  // }
+  if (this._state === "pending") {
+    this._state = "fulfilled";
+    this._value = data;
+  }
+};
+
+$Promise.prototype._internalReject = function (data) {
+  if (this._state === "pending") {
+    this._state = "rejected";
+    this._value = data;
+  }
+};
+
+// const prub = new $Promise(() => {});
 
 module.exports = $Promise;
 /*-------------------------------------------------------
