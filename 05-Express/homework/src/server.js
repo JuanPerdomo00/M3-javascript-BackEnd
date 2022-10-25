ce7e47e42ac69c5728a46237b94189afb4192734
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-24 22:35:06 -0500

    Termine la mitad de test, los revisare luego con la CR

--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -1,4 +1,4 @@
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const STATUS_USER_ERROR = 422;
@@ -9,9 +9,96 @@ const posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
-// server.use(express.json());
+server.use(express.json());
+// server.use(bodyParser.json())
 
 // TODO: your code to handle requests
 
+
+
+server.get("/posts/:author/:title", (req, res) => {
+
+})
+
+
+
+server.get("/posts/:author", (req, res) => {
+  let { author } = req.params;
+
+  const autor = posts.filter((post) =>
+    post.title && post.contents ? post.author === author : null
+  );
+  if (autor.length === 0) {
+    res
+      .status(STATUS_USER_ERROR)
+      .json({ error: "No existe ningun post del autor indicado" });
+  }
+  return res.json(autor);
+});
+
+server.get("/posts", (req, res) => {
+  // let { term } = req.params;
+  // let { url } = req;
+  // let { title, contents } = req.query;
+  // let urlArr = url.split("/");
+
+  // if (urlArr[2] === term) {
+  //   res.json({
+  //     title: term[title],
+  //     contents: term[contents],
+  //   });
+  //}
+
+  // res.json(posts.forEach((post) => post));
+
+  let { term } = req.query;
+
+  //? term -> titulo || term -> contents
+  //! first title
+  const termin = posts.filter(
+    (post) => post.title.includes(term) || post.contents.includes(term)
+  );
+  if (termin) {
+    res.json(termin);
+  }
+  res.json(posts);
+});
+
+server.post("/posts", (req, res) => {
+  let { author, title, contents } = req.body;
+  let obj;
+  if (!author || !title || !contents) {
+    res.status(STATUS_USER_ERROR).json({
+      error: "No se recibieron los parámetros necesarios para crear el Post",
+    });
+  }
+  obj = {
+    id: 1,
+    author,
+    title,
+    contents,
+  };
+  posts.push(obj);
+  res.json(obj);
+});
+
+server.post("/posts/author/:author", (req, res) => {
+  let { author } = req.params;
+  let { title, contents } = req.body;
+  if (!author || !title || !contents) {
+    res.status(STATUS_USER_ERROR).json({
+      error: "No se recibieron los parámetros necesarios para crear el Post",
+    });
+  }
+
+  obj = {
+    id: 1,
+    author,
+    title,
+    contents,
+  };
+  posts.push(obj);
+  res.json(obj);
+});
 
 module.exports = { posts, server };
