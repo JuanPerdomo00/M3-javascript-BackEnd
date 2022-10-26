fa1d5b01922bece57e2710834bbf5d363c9a58a1
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-25 21:33:14 -0500

    pendiente terminar en la CR

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -1,18 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json()); // for parsing application/json
 
-app.get('/', (req, res) => {
-  res.send({
-    message: 'hola',
+app.get("/", (req, res) => {
+  res.json({
+    message: "hola",
   });
 });
 
-app.post('/product', (req, res) => {
-  res.send({
+app.get("/test", (req, res) => {
+  res.json({
+    message: "test",
+  });
+});
+
+app.post("/product", (req, res) => {
+  res.json({
     result: req.body.a / req.body.b,
   });
+});
+
+app.post("/sum", (req, res) => {
+  const { a, b } = req.body;
+  let sum = Number(a + b);
+
+  if (sum !== NaN) {
+    res.sendStatus(200);
+    // console.log(typeof sum)
+    res.json({
+      result: sum,
+    });
+  }
 });
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
