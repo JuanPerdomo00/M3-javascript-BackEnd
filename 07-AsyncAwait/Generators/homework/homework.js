8b379dfa316aacef35f908e86a2fa72b628ce1f9
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-10-27 11:11:20 -0500

    se termino la home a mi maner 'larguisimagit add homework.js!'

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,34 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max = true) {
   // Tu código acá:
-
+  if (typeof max === 'boolean') {
+    let i = 1;
+    while (max) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        yield "Fizz Buzz";
+      } else if (i % 3 === 0) {
+        yield "Fizz";
+      } else if (i % 5 === 0) {
+        yield "Buzz";
+      } else {
+        yield i;
+      }
+      i++;
+    }
+  } else {
+    let i = 1;
+    while (i <= max) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        yield "Fizz Buzz";
+      } else if (i % 3 === 0) {
+        yield "Fizz";
+      } else if (i % 5 === 0) {
+        yield "Buzz";
+      } else {
+        yield i;
+      }
+      i++;
+    }
+  }
 }
 
 module.exports = fizzBuzzGenerator;
